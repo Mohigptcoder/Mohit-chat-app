52f573ee7cb4cd08c75f43f5a87fb904873866b6
Mohigptcoder <humk18655@gmail.com>
2026-07-30 13:54:03 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const app = document.getElementById("app");
+
+app.innerHTML = `
+<div style="
+display:flex;
+justify-content:center;
+align-items:center;
+height:100vh;
+font-size:28px;
+font-weight:bold;
+">
+🚀 Mohit Chat is Loading...
+</div>
+`;
